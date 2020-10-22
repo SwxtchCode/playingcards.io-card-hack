f39ddaa987b2d1646e3f1a4adf0436668318bae8
SwxtchCode <swxtchcode@protonmail.com>
2020-10-22 15:10:39 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,23 @@
 setInterval(() => {
     for (i = 0; i < $(".Card--facedown").length; i++) {
         elem = $(".Card--facedown")[i];
-
+ 
         elem.children[1].children[0].children[0].innerText = "";
     }
-
+ 
     for (i = 0; i < $(".Card--facedown").length; i++) {
         elem = $(".Card--facedown")[i];
-
-        cardNum = elem.children[0].children[0].children[0].style.backgroundImage.replaceAll(`url("`, "").replaceAll(`"`, "").replaceAll(`)`, "").replaceAll(`/img/cards/`, "").replaceAll(`.svg`, "").replace("-", " ");
-
+ 
+        cardNum = elem.children[0].children[0].children[0].style.backgroundImage
+            .replaceAll(`url("`, "")
+            .replaceAll(`"`, "")
+            .replaceAll(`)`, "")
+            .replaceAll(`/img/cards/`, "")
+            .replaceAll("/img/cards-french", "")
+            .replaceAll(`.svg`, "")
+            .replace("-", " ")
+            .replaceAll("/", "");
+ 
         elem.children[1].children[0].children[0].innerText = cardNum;
     }
 }, 1000);
