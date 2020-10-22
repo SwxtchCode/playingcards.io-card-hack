889da042ccce6e921eeb843ddaa988ed4097476f
SwxtchCode <swxtchcode@protonmail.com>
2020-10-21 20:24:56 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,15 @@
 setInterval(() => {
-     for (i = 0; i < $(".Card--facedown").length; i++) {
+    for (i = 0; i < $(".Card--facedown").length; i++) {
         elem = $(".Card--facedown")[i];
- 
-       elem.children[1].children[0].children[0].innerText = "";
-     }
 
-     for (i = 0; i < $(".Card--facedown").length; i++) {
-         elem = $(".Card--facedown")[i];
- 
-          cardNum = elem.children[0].children[0].children[0].style.backgroundImage
-              .replaceAll(`url("`, "")
-              .replaceAll(`"`, "")
-              .replaceAll(`)`, "")
-              .replaceAll(`/img/cards/`, "")        
-              .replaceAll(`.svg`, "")
-              .replace("-", " ");
+        elem.children[1].children[0].children[0].innerText = "";
+    }
+
+    for (i = 0; i < $(".Card--facedown").length; i++) {
+        elem = $(".Card--facedown")[i];
+
+        cardNum = elem.children[0].children[0].children[0].style.backgroundImage.replaceAll(`url("`, "").replaceAll(`"`, "").replaceAll(`)`, "").replaceAll(`/img/cards/`, "").replaceAll(`.svg`, "").replace("-", " ");
 
         elem.children[1].children[0].children[0].innerText = cardNum;
     }
-}, 1000)
+}, 1000);
